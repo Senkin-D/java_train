1b5eb955b458f4d9a83dc98aba789a4f7c273ede
Senkin-D <54044697+Senkin-D@users.noreply.github.com>
2021-06-20 23:23:07 +0300

    Файл перемещен в пакет

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -1,6 +1,17 @@
-public class MyFirstProg {
+package ru.stqa.pft.sandbox;
+
+public class MFP {
 
 	public static void main(String[] args){
+
+
+
+
 		System.out.println("Hello world");
 	}
+
+
+
+
+
 }
