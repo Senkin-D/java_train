5a932f447d46537fadf175ecdd234aa6b5faa587
Senkin-D <54044697+Senkin-D@users.noreply.github.com>
2021-06-22 01:00:58 +0300

    Плюс пара простых функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -1,13 +1,32 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class MFP {
 
-	public static void main(String[] args){
-		System.out.println("Hello world");
-	}
+    public static void main(String[] args) {
+        hello("world");
+        double l = 5;
+        System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("площадь прям-ка со сторонами " + a + " и " + b + " = " + area(a,b));
 
 
+    }
 
 
+    public static void hello(String smbd) {
+        System.out.println("Hello " + smbd + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 
 }
